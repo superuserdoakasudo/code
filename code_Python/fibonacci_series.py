c3c9b48116b8423ab6c6d4848875c2ae44976262
ri <22332371@student.uwa.edu.au>
2023-05-17 08:54:27 +0800

    Add fibonacci_series.py file

---
+++ code_Python/fibonacci_series.py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number of elements: "))
+n1, n2 = 0, 1
+print(n1, n2, end=" ")
+
+for i in range(2, number):
+    n3 = n1 + n2
+    print(n3, end=" ")
+    n1, n2 = n2, n3
